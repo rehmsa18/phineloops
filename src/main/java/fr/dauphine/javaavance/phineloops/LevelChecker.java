565afeb10daa857a1508ba0c32ba307533b47d31
Sania Ur Rehman <sania@prestachef.fr>
2019-12-06 21:30:25 +0100

    add level checker

---
+++ src/main/java/fr/dauphine/javaavance/phineloops/LevelChecker.java
@@ -0,0 +1,50 @@
+package fr.dauphine.javaavance.phineloops;
+
+import java.io.IOException;
+
+public class LevelChecker {
+	
+	public static boolean checker() throws IOException {
+		
+		Grid  grid= Grid.readFile("a");
+
+		grid.displayInConsole();		
+		
+		for(int i=0; i<grid.height; i++){
+			for(int j=0; j<grid.width; j++){
+				
+				//verify the pieces in the northern border
+				if (!grid.existsPiece(i-1,j) && grid.cases[i][j].links[0] == 1){
+					return false;
+				}
+				
+				//check the pieces on the eastern border
+				if(!grid.existsPiece(i,j+1) && grid.cases[i][j].links[1] == 1) {
+					return false;
+				}
+			
+				//check the pieces on the southern border
+				if(!grid.existsPiece(i+1,j) && grid.cases[i][j].links[2] == 1) {
+					return false;
+				}
+				
+				//check the pieces on the western border
+				if(!grid.existsPiece(i,j-1) && grid.cases[i][j].links[3] == 1) {
+					return false;
+				}
+				
+				//check the pieces in the center
+				if(!grid.allLinked(grid.cases[i][j])) {
+					return false;
+				}
+			}
+			
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(LevelChecker.checker());
+	}
+
+}

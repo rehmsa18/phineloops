0da247bc4a3a563072483b69fe63afc163cd6b70
Sania Ur Rehman <sania@prestachef.fr>
2019-12-20 22:19:25 +0100

    Test public instances with solver

---
+++ src/main/java/fr/dauphine/javaavance/phineloops/utils/TestInstances.java
@@ -0,0 +1,36 @@
+package fr.dauphine.javaavance.phineloops.utils;
+
+import java.io.File;
+import java.io.IOException;
+import fr.dauphine.javaavance.phineloops.levelFunctions.LevelGenerator;
+import fr.dauphine.javaavance.phineloops.levelFunctions.LevelSolverIA;
+import fr.dauphine.javaavance.phineloops.levelFunctions.LevelSolverStack;
+import fr.dauphine.javaavance.phineloops.model.Grid;
+import fr.dauphine.javaavance.phineloops.view.LevelDisplay;
+
+public class TestInstances {
+
+	public static void test() throws IOException {
+		final File folder = new File(".\\instances\\public");
+		long totalTime = 0;
+	    for (final File fileName : folder.listFiles()) {
+	    	System.out.println( );
+	    	Grid grid = Read.readFile(fileName.toString());
+	    	
+			long debut = System.currentTimeMillis();
+			LevelSolverIA sol = new LevelSolverIA(grid, 1);
+			//LevelSolverStack sol = new LevelSolverStack(grid, true);
+			boolean response = sol.solve();
+			long fin = System.currentTimeMillis();
+			long time = fin-debut;
+			
+			System.out.println(fileName + " " + response + " " + time);
+			totalTime += time;
+	    }  
+	    System.out.println(totalTime);
+	}
+	public static void main(String[] args) throws IOException {
+		TestInstances.test();
+	}
+
+}
